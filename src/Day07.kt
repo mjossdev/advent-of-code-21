8fb1e52aa817a33350b772f2509c04dddf54cd80
Marcel Joss <marceljoss@hotmail.ch>
2021-12-07 13:05:17 +0100

    Use non null assertions

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,15 +1,17 @@
 import kotlin.math.abs
 
 fun main() {
+
+
     fun part1(input: List<Int>): Int {
-        val min = input.minOrNull() ?: throw IllegalArgumentException()
-        val max = input.maxOrNull() ?: throw IllegalArgumentException()
+        val min = input.minOrNull()!!
+        val max = input.maxOrNull()!!
         val range = min..max
-        return range.minOfOrNull { pos -> input.sumOf { abs(it - pos) } } ?: throw AssertionError()
+        return range.minOfOrNull { pos -> input.sumOf { abs(it - pos) } }!!
     }
     fun part2(input: List<Int>): Int {
-        val min = input.minOrNull() ?: throw IllegalArgumentException()
-        val max = input.maxOrNull() ?: throw IllegalArgumentException()
+        val min = input.minOrNull()!!
+        val max = input.maxOrNull()!!
         val range = min..max
         return range.minOfOrNull { pos ->
             input.sumOf {
@@ -17,7 +19,7 @@ fun main() {
                 // Gauss Sum
                 (n * n + n) / 2
             }
-        } ?: throw AssertionError()
+        }!!
     }
 
     val testInput = readInputAsInts("Day07_test")

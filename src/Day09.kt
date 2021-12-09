b658f5a223e4654a97d75a753cfed2b309c75092
Marcel Joss <marceljoss@hotmail.ch>
2021-12-09 18:41:54 +0100

    Day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,25 +1,66 @@
-import kotlin.math.max
-import kotlin.math.min
-
 fun main() {
-    fun part1(input: List<List<Int>>): Int =
-        input.flatMapIndexed { rowIndex, row ->
-            row.filterIndexed { colIndex, height ->
-                val rowRange = max(0, rowIndex - 1)..min(rowIndex + 1, input.lastIndex)
-                val colRange = max(0, colIndex - 1)..min(colIndex + 1, row.lastIndex)
-                rowRange.all { r -> r == rowIndex || height < input[r][colIndex]} && colRange.all { c -> c == colIndex || height < input[rowIndex][c]}
+    data class Coordinate(val row: Int, val col: Int)
+
+    operator fun <T> List<List<T>>.get(coordinate: Coordinate): T = this[coordinate.row][coordinate.col]
+
+    fun getLowPoints(heightmap: List<List<Int>>): List<Coordinate> {
+        val lowPoints = mutableListOf<Coordinate>()
+        heightmap.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { colIndex, height ->
+                val rowRange = rowIndex - 1..rowIndex + 1 step 2
+                val colRange = colIndex - 1..colIndex + 1 step 2
+                if (colRange.all { height < row.getOrElse(it) { 10 } } &&
+                    rowRange.all { height < (heightmap.getOrNull(it)?.getOrNull(colIndex) ?: (10)) }) {
+                    lowPoints.add(Coordinate(rowIndex, colIndex))
+                }
             }
-        }.also { println(it) }.sumOf {
-            it + 1
+        }
+        return lowPoints
+    }
+
+    fun collectBasin(heightmap: List<List<Int>>, coordinate: Coordinate, target: MutableSet<Coordinate>) {
+        if (heightmap[coordinate] == 9 || coordinate in target) {
+            return
+        }
+        target.add(coordinate)
+        val row = heightmap[coordinate.row]
+        if (coordinate.row > 0) {
+            collectBasin(heightmap, coordinate.copy(row = coordinate.row - 1), target)
+        }
+        if (coordinate.row < heightmap.lastIndex) {
+            collectBasin(heightmap, coordinate.copy(row = coordinate.row + 1), target)
+        }
+        if (coordinate.col > 0) {
+            collectBasin(heightmap, coordinate.copy(col = coordinate.col - 1), target)
+        }
+        if (coordinate.col < row.lastIndex) {
+            collectBasin(heightmap, coordinate.copy(col = coordinate.col + 1), target)
+        }
+    }
+
+    fun getBasins(heightmap: List<List<Int>>): List<Set<Coordinate>> =
+        getLowPoints(heightmap).map {
+            val basin = mutableSetOf<Coordinate>()
+            collectBasin(heightmap, it, basin)
+            basin
         }
 
-    val testInput = readInput("Day09_test").map { l ->
+    fun part1(input: List<List<Int>>): Int =
+        getLowPoints(input).asSequence().map { input[it.row][it.col] }.sumOf { it + 1 }
+
+    fun part2(input: List<List<Int>>): Int =
+        getBasins(input).asSequence().map { it.size }.sortedDescending().take(3).reduce(Int::times)
+
+    fun readHeightmap(name: String): List<List<Int>> = readInput(name).map { l ->
         l.map { it.digitToInt() }
     }
-    val input = readInput("Day09").map { l ->
-        l.map { it.digitToInt() }
-    }
+
+    val testInput = readHeightmap("Day09_test")
+    val input = readHeightmap("Day09")
 
     check(part1(testInput) == 15)
     println(part1(input))
+
+    check(part2(testInput) == 1134)
+    println(part2(input))
 }

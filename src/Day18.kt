628c5a80aec8bdad2f56ebe3e290c37b9307fb16
Marcel Joss <marceljoss@hotmail.ch>
2021-12-18 22:12:54 +0100

    Use polymorphism instead of type checking

--- src/Day18.kt
+++ src/Day18.kt
@@ -3,185 +3,164 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonPrimitive
 
-private sealed class ReduceResult(val number: SnailfishNumber)
-
-private class UnchangedResult(number: SnailfishNumber) : ReduceResult(number)
-
-private class ChangedResult(number: SnailfishNumber) : ReduceResult(number)
-
-private class ExplodeResult(number: SnailfishNumber, val left: Int?, val right: Int?) : ReduceResult(number) {
-    fun withNumber(number: SnailfishNumber) = ExplodeResult(number, left, right)
+private sealed interface ReduceResult {
+    val node: Node
 }
 
-private class SnailfishNumber {
-    val value: Any
+private data class UnchangedResult(override val node: Node) : ReduceResult
 
-    constructor(number: Int) {
-        value = number
-    }
+private data class ChangedResult(override val node: Node) : ReduceResult
 
-    constructor(left: SnailfishNumber, right: SnailfishNumber) {
-        value = Pair(left, right)
-    }
+private data class ExplodeResult(override val node: Node, val left: Int?, val right: Int?) : ReduceResult
 
-    @Suppress("UNCHECKED_CAST")
-    fun asPair(): Pair<SnailfishNumber, SnailfishNumber> {
-        check(value is Pair<*, *>)
-        return value as Pair<SnailfishNumber, SnailfishNumber>
-    }
-
-    fun reduce(): SnailfishNumber {
+private sealed class Node {
+    fun reduce(): Node {
         var current = this
         do {
             var reduceResult = current.explode()
             if (reduceResult is UnchangedResult) {
                 reduceResult = current.split()
             }
-            current = reduceResult.number
+            current = reduceResult.node
         } while (reduceResult !is UnchangedResult)
         return current
     }
 
-    private fun split(): ReduceResult {
-        when (value) {
-            is Int -> {
-                if (value > 9) {
-                    val left = value / 2
-                    val right = (value + 1) / 2
-                    val number = SnailfishNumber(SnailfishNumber(left), SnailfishNumber(right))
-                    return ChangedResult(number)
-                }
-            }
-            is Pair<*, *> -> {
-                val (left, right) = asPair()
-                val leftResult = left.split()
-                if (leftResult is ChangedResult) {
-                    return ChangedResult(SnailfishNumber(leftResult.number, right))
-                }
-                val rightResult = right.split()
-                if (rightResult is ChangedResult) {
-                    return ChangedResult(SnailfishNumber(left, rightResult.number))
-                }
-            }
+    abstract fun split(): ReduceResult
+    abstract fun explode(level: Int = 0): ReduceResult
+    abstract fun addLeftMost(number: Int): ReduceResult
+    abstract fun addRightMost(number: Int): ReduceResult
+
+    abstract val magnitude: Int
+}
+
+private data class NumberNode(val number: Int): Node() {
+    override fun split(): ReduceResult =
+        if (number > 9) {
+            val left = NumberNode(number / 2)
+            val right = NumberNode((number + 1) / 2)
+            ChangedResult(PairNode(left, right))
+        } else {
+            UnchangedResult(this)
         }
-        return UnchangedResult(this)
-    }
 
-    private fun explode(level: Int = 0): ReduceResult {
-        if (value is Pair<*, *>) {
-            val (left, right) = asPair()
-            if (level > 3) {
-                val number = SnailfishNumber(0)
-                return ExplodeResult(number, left.value as Int, right.value as Int)
-            }
-            val leftResult = left.explode(level + 1)
-            if (leftResult is ExplodeResult) {
-                leftResult.right?.also {
-                    val addResult = right.addLeftMost(it)
-                    if (addResult is ChangedResult) {
-                        return ExplodeResult(
-                            SnailfishNumber(leftResult.number, addResult.number),
-                            leftResult.left,
-                            null
-                        )
-                    }
-                }
-                return leftResult.withNumber(SnailfishNumber(leftResult.number, right))
-            }
-            val rightResult = right.explode(level + 1)
-            if (rightResult is ExplodeResult) {
-                rightResult.left?.also {
-                    val addResult = left.addRightMost(it)
-                    if (addResult is ChangedResult) {
-                        return ExplodeResult(
-                            SnailfishNumber(addResult.number, rightResult.number),
-                            null,
-                            rightResult.right
-                        )
-                    }
-                }
-                return rightResult.withNumber(SnailfishNumber(left, rightResult.number))
-            }
-        }
-        return UnchangedResult(this)
-    }
+    override fun explode(level: Int): ReduceResult = UnchangedResult(this)
 
-    fun addLeftMost(number: Int): ReduceResult {
-        when (value) {
-            is Int -> return ChangedResult(SnailfishNumber(value + number))
-            else -> {
-                val (left, right) = asPair()
-                val leftResult = left.addLeftMost(number)
-                if (leftResult is ChangedResult) {
-                    return ChangedResult(SnailfishNumber(leftResult.number, right))
-                }
-                val rightResult = right.addLeftMost(number)
-                if (rightResult is ChangedResult) {
-                    return ChangedResult(SnailfishNumber(left, rightResult.number))
-                }
-                return UnchangedResult(this)
-            }
-        }
-    }
+    override fun addLeftMost(number: Int): ReduceResult = add(number)
 
-    fun addRightMost(number: Int): ReduceResult {
-        when (value) {
-            is Int -> return ChangedResult(SnailfishNumber(value + number))
-            else -> {
-                val (left, right) = asPair()
-                val rightResult = right.addRightMost(number)
-                if (rightResult is ChangedResult) {
-                    return ChangedResult(SnailfishNumber(left, rightResult.number))
-                }
-                val leftResult = left.addRightMost(number)
-                if (leftResult is ChangedResult) {
-                    return ChangedResult(SnailfishNumber(leftResult.number, right))
-                }
-                return UnchangedResult(this)
-            }
-        }
-    }
+    override fun addRightMost(number: Int): ReduceResult = add(number)
 
-    operator fun plus(other: SnailfishNumber) = SnailfishNumber(this, other).reduce()
+    private fun add(number: Int): ChangedResult = ChangedResult(NumberNode(this.number + number))
 
-    fun magnitude(): Int = when (value) {
-        is Int -> value
-        else -> {
-            val (left, right) = asPair()
-            3 * left.magnitude() + 2 * right.magnitude()
-        }
-    }
+    override val magnitude: Int get() = number
 
-    override fun toString() = if (value is Pair<*, *>) "[${value.first},${value.second}]" else value.toString()
+    override fun toString() = number.toString()
 
     companion object {
-        fun fromJson(json: JsonElement): SnailfishNumber = when (json) {
-            is JsonPrimitive -> SnailfishNumber(json.asInt)
-            is JsonArray -> {
-                check(json.size() == 2)
-                val left = fromJson(json[0])
-                val right = fromJson(json[1])
-                SnailfishNumber(left, right)
-            }
-            else -> error("Unsupported type")
-        }
+        val ZERO = NumberNode(0)
     }
 }
 
-fun main() {
-    fun readPuzzleInput(name: String): List<SnailfishNumber> {
-        val gson = Gson()
-        return readInput(name).map {
-            val json = gson.fromJson(it, JsonElement::class.java)
-            SnailfishNumber.fromJson(json)
+private data class PairNode(val left: Node, val right: Node) : Node() {
+    override fun split(): ReduceResult {
+        val leftResult = left.split()
+        if (leftResult is ChangedResult) {
+            return ChangedResult(copy(left = leftResult.node))
         }
+        val rightResult = right.split()
+        if (rightResult is ChangedResult) {
+            return ChangedResult(copy(right = rightResult.node))
+        }
+        return UnchangedResult(this)
     }
 
-    fun part1(input: List<SnailfishNumber>) = input.reduce { l, r -> (l + r) }.magnitude()
+    override fun explode(level: Int): ReduceResult {
+        if (level > 3) {
+            check(left is NumberNode && right is NumberNode)
+            return ExplodeResult(NumberNode.ZERO, left.number, right.number)
+        }
+        val leftResult = left.explode(level + 1)
+        if (leftResult is ExplodeResult) {
+            leftResult.right?.also {
+                val addResult = right.addLeftMost(it)
+                if (addResult is ChangedResult) {
+                    return leftResult.copy(
+                        node = PairNode(leftResult.node, addResult.node),
+                        right = null
+                    )
+                }
+            }
+            val node = copy(left = leftResult.node)
+            return leftResult.copy(node = node)
+        }
+        val rightResult = right.explode(level + 1)
+        if (rightResult is ExplodeResult) {
+            rightResult.left?.also {
+                val addResult = left.addRightMost(it)
+                if (addResult is ChangedResult) {
+                    return rightResult.copy(
+                        node = PairNode(addResult.node, rightResult.node),
+                        left = null
+                    )
+                }
+            }
+            val node = copy(right = rightResult.node)
+            return rightResult.copy(node = node)
+        }
+        return UnchangedResult(this)
+    }
 
-    fun part2(input: List<SnailfishNumber>): Int = input.flatMap { a ->
+    override fun addLeftMost(number: Int): ReduceResult {
+        val leftResult = left.addLeftMost(number)
+        if (leftResult is ChangedResult) {
+            return ChangedResult(copy(left = leftResult.node))
+        }
+        val rightResult = right.addLeftMost(number)
+        if (rightResult is ChangedResult) {
+            return ChangedResult(copy(right = rightResult.node))
+        }
+        return UnchangedResult(this)
+    }
+
+    override fun addRightMost(number: Int): ReduceResult {
+        val rightResult = right.addRightMost(number)
+        if (rightResult is ChangedResult) {
+            return ChangedResult(copy(right = rightResult.node))
+        }
+        val leftResult = left.addRightMost(number)
+        if (leftResult is ChangedResult) {
+            return ChangedResult(copy(left = leftResult.node))
+        }
+        return UnchangedResult(this)
+    }
+
+    override val magnitude: Int get() = 3 * left.magnitude + 2 * right.magnitude
+
+    override fun toString() = "[$left,$right]"
+}
+
+private operator fun Node.plus(other: Node) = PairNode(this, other).reduce()
+
+private fun JsonElement.toNode(): Node = when (this) {
+    is JsonPrimitive -> NumberNode(asInt)
+    is JsonArray -> {
+        check(size() == 2)
+        PairNode(this[0].toNode(), this[1].toNode())
+    }
+    else -> error("Unsupported JSON type")
+}
+
+fun main() {
+    fun readPuzzleInput(name: String): List<Node> {
+        val gson = Gson()
+        return readInput(name).map { gson.fromJson(it, JsonElement::class.java).toNode() }
+    }
+
+    fun part1(input: List<Node>) = input.reduce { l, r -> (l + r) }.magnitude
+
+    fun part2(input: List<Node>): Int = input.flatMap { a ->
         input.asSequence().filter { a != it }.map {
-            (a + it).magnitude()
+            (a + it).magnitude
         }
     }.max()
 
